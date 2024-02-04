ea3a48f2d66658535f7fdf042f5cf3a78a5ec95f
黄仁宁 <huangrenning@winbulls.com>
2024-02-04 14:23:11 +0800

    feat: 接入winbull open ai 代理

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 
 load_dotenv('api.env')
 os.environ['OPENAI_API_KEY'] = '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'
-os.environ['OPENAI_BASE_URL'] = f"https://api.openai-proxy.com/v1"
+os.environ['OPENAI_BASE_URL'] = f"http://107.23.200.130/v1"
 
 #
 llm = ChatOpenAI(model_name='gpt-3.5-turbo-1106')
